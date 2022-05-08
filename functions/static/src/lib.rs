567515dc6253367274ba289298a64310412c0e1c
Mitchell Hynes <me@mitchellhynes.com>
2022-05-08 20:24:26 -0230

    Progress on mimetypes

--- functions/static/src/lib.rs
+++ functions/static/src/lib.rs
@@ -13,7 +13,10 @@ fn serve(req: Request) -> Result<Response> {
     let path = req.headers().get("spin-path-info").unwrap().to_str()?;
 
     match read(path) {
-        Ok(body) => Ok(http::Response::builder().status(200).body(Some(body))?),
+        Ok((mime, body)) => Ok(http::Response::builder()
+            .status(200)
+            .header("Content-Type", mime)
+            .body(Some(body))?),
         Err(err) => {
             eprintln!("Error: {}", err);
             spin_sdk::http::not_found()
@@ -21,8 +24,10 @@ fn serve(req: Request) -> Result<Response> {
     }
 }
 
+const TEXT_PLAIN: &'static str = "text/html";
+
 /// Open the file given its path and return its content.
-fn read(path: &str) -> Result<Bytes> {
+fn read(path: &str) -> Result<(String, Bytes)> {
     let path_obj = Path::new(path);
     let mut file = if path_obj.is_dir() {
         File::open(path_obj.join("index.html"))
@@ -34,5 +39,11 @@ fn read(path: &str) -> Result<Bytes> {
     let mut buf = vec![];
     file.read_to_end(&mut buf)?;
 
-    Ok(buf.into())
+    Ok((
+        mime_guess::from_path(path_obj)
+            .first()
+            .map(|mime| mime.essence_str().to_owned())
+            .unwrap_or(TEXT_PLAIN.to_owned()),
+        buf.into(),
+    ))
 }
