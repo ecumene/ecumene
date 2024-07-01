6ffd6fd982a01f3f3e926eaa1b503588f98ddc8d
ecumene <mitch@spellbook.legal>
2024-07-01 02:07:35 -0230

    Astro deploy GH actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://mitchellhynes.com",
   integrations: [mdx(), sitemap(), tailwind(), react()],
 });
