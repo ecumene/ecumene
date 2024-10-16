4a04473d09abc134d31dc72986d8131a2fbe0817
Mitchell Hynes <me@mitchellhynes.com>
2024-10-15 23:50:13 -0230

    Monorepo init

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,19 @@
+import MyHead from "./icons/MyHead";
+import MySignature from "./icons/MySignature";
+import Shape from "./icons/Shape";
+import CardFan from "./ui/cardFan";
+import JesterButton from "./ui/jesterbutton";
+import ProjectTable from "./ui/projectTable";
+import Skeleton from "./ui/splain";
+import Webpage from "./ui/webpage";
+
+export {
+  MyHead,
+  MySignature,
+  CardFan,
+  ProjectTable,
+  JesterButton,
+  Skeleton,
+  Webpage,
+  Shape,
+};
