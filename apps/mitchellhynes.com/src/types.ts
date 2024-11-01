0c6b5cd56c64a31a04874ffbc402934752780553
Mitchell Hynes <me@mitchellhynes.com>
2024-11-01 21:13:18 -0230

    Project card redesign

---
+++ apps/mitchellhynes.com/src/types.ts
@@ -0,0 +1,7 @@
+export type Project = {
+  date: string;
+  projectName: string;
+  description: string;
+  link: string;
+  technologies: string;
+};
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,7 +1,6 @@
 import JesterButton from "./ui/AnimatedJesterButton";
-import ProjectTable from "./ui/projectTable";
 import IOSMessageDisplay from "./ui/IOSMessageDisplay";
 import Card from "./ui/Card";
 import WebpageDisplay from "./ui/WebpageDisplay";
 
-export { ProjectTable, JesterButton, IOSMessageDisplay, WebpageDisplay, Card };
+export { JesterButton, IOSMessageDisplay, WebpageDisplay, Card };
