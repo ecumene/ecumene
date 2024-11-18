b38f0a40652c1bd82482f60430051f0ec83d0283
Mitchell Hynes <me@mitchellhynes.com>
2024-11-17 23:05:15 -0330

    Jesters privilege styles

---
+++ apps/jestersprivilege.ca/tailwind.config.ts
@@ -0,0 +1,105 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "../../node_modules/@ecumene/ui/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      serif: ["EB Garamond", "serif"],
+      display: ["UnifrakturCook", "serif"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        terracotta: {
+          50: "#aa635a",
+          100: "#5e3631",
+          200: "#4f2d29",
+        },
+        cream: {
+          50: "#f2f0dd",
+          100: "#e4e1c9",
+          300: "#c6c3af",
+        },
+        gray: {
+          800: "#121213",
+        },
+        emerald: {
+          600: "#0c352d",
+        },
+        gold: {
+          50: "#fff0ba",
+          100: "#cfb65a",
+          400: "#746428",
+        },
+        red: {
+          100: "#531818",
+        },
+        blue: {
+          100: "#082b3b",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+};
+
+export default config;
