ae75ecc63ff19398f8b7578413040c94d5203b14
Mitchell Hynes <me@mitchellhynes.com>
2025-02-06 22:29:05 -0330

    Progress on writing

--- apps/jestersprivilege.ca/astro.config.mjs
+++ apps/jestersprivilege.ca/astro.config.mjs
@@ -8,5 +8,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
 });
